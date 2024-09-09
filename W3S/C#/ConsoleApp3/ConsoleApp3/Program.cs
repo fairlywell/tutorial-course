7b2871b55de2536d8f311e325753ed63965a7609
humhliao <humhliao@gmail.com>
2024-09-09 15:00:58 +0700

    Variable 3

---
+++ W3S/C#/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,12 @@
+﻿string name = "john";
+Console.WriteLine("Hello" + name);
+string firstName = "John ";
+string lastName = "Doe";
+string fullName = firstName + lastName;
+Console.WriteLine(fullName);
+
+int x = 5;
+int y = 7;
+Console.WriteLine(x + y);
+
+Console.ReadKey();
