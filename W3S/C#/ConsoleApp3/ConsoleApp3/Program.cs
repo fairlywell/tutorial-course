2bc6832b9070e12d2ebe1c0bb2686ed2d0e6513d
humhliao <humhliao@gmail.com>
2024-09-09 20:56:24 +0700

    Arithmetic practice

--- W3S/C#/ConsoleApp3/ConsoleApp3/Program.cs
+++ W3S/C#/ConsoleApp3/ConsoleApp3/Program.cs
@@ -5,8 +5,11 @@ string lastName = "Doe";
 string fullName = firstName + lastName;
 Console.WriteLine(fullName);
 
-int x = 5;
-int y = 72;
-Console.WriteLine(x + y);
+//int x = 5;
+//int y = 72;
+//Console.WriteLine(x + y);
+
+int x = 5, y = 2, z = 1;
+Console.WriteLine(x + y + z);
 
 Console.ReadKey();
