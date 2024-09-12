807212063c4c8ee9df6e99421ee6d71a4f9a82e1
humhliao <humhliao@gmail.com>
2024-09-12 08:04:54 +0700

    Practice 14 js

---
+++ Bro Code course/JavaScript/Tutorial/14/index.js
@@ -0,0 +1,26 @@
+// let userName = "BroCode";
+
+
+// userName.indexOf("o");
+// console.log(userName.indexOf("o"));
+// console.log(userName.lastIndexOf("o"));
+// console.log(userName.length);
+
+// // userName = userName.repeat(3);
+
+// // console.log(userName);
+// let result = userName.includes(" ");
+
+// // console.log(result);
+// if(result) {
+//   console.log("Your username can't include ' '");
+  
+// } else {
+//   console.log(userName);
+  
+// }
+
+let phoneNumber = "123-456-7890";
+phoneNumber = phoneNumber.padEnd(20, "0");
+
+console.log(phoneNumber);
