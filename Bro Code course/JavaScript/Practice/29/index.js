45899ca0672f077ba13b4476999cfe7c7b3423a7
humhliao <humhliao@gmail.com>
2024-09-12 21:32:10 +0700

    Practice 30 js update

--- Bro Code course/JavaScript/Practice/29/index.js
+++ Bro Code course/JavaScript/Practice/29/index.js
@@ -1,4 +1,5 @@
 function rollDice() {
+  // khoi tao lay gia tri value tu input
   const numOfDice = document.getElementById("numOfDice").value;
   const diceResult = document.getElementById("diceResult");
   const diceImages = document.getElementById("diceImages");
@@ -7,11 +8,14 @@ function rollDice() {
   const images = [];
 
   for(let i = 0 ; i < numOfDice; i++) {
+    //lap tu so nhap vao
     const value = Math.floor(Math.random() * 6) + 1;
+    // them value vao hang cuoi values
     values.push(value);
+    // them anh vao mang images
     images.push(`<img src="dice_images/Dice-${value}.svg.png" alt="Dice-${values}">`);
   }
-  
+  // hien thi ket qua dung join gop ket qua tung xac
   diceResult.textContent = `You roll to ${values.join(', ')}`;
   diceImages.innerHTML = images.join('');
 }
