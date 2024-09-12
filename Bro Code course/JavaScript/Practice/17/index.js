c127c873032565aa761237ee18be49fb1e54270b
humhliao <humhliao@gmail.com>
2024-09-12 09:50:36 +0700

    Practice 17 js

---
+++ Bro Code course/JavaScript/Practice/17/index.js
@@ -0,0 +1,26 @@
+const temp = 26;
+
+// if(temp >= 0 && temp <= 30) {
+//   console.log("Weather is Good");
+// } else {
+//   console.log("Weather is Bad");
+  
+// 
+
+if (temp < 0 || temp > 30) {
+  console.log("Weather is Bad");
+  
+} else {
+  console.log("Weather is Good");
+  
+}
+
+const isSunny = false;
+
+if (!isSunny) {
+  console.log("Sunny");
+  
+} else {
+  console.log("CLoudy");
+  
+}
