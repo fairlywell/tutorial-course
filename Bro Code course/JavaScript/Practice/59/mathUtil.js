aa27975bf01b1e7fd43ac7368115057b30bb30d5
humhliao <humhliao@gmail.com>
2024-09-15 10:42:30 +0700

    Practice 59 js

---
+++ Bro Code course/JavaScript/Practice/59/mathUtil.js
@@ -0,0 +1,14 @@
+// xuat export moi ham ma can nhap de xuat ra file khac
+export const PI = 3.14159;
+
+export function getCircumference(radius) {
+  return 2 * PI * radius;
+}
+
+export function getArea(radius) {
+  return PI * radius * radius;
+}
+
+export function getVolume(radius) {
+  return 4 * PI * radius * radius;
+}
