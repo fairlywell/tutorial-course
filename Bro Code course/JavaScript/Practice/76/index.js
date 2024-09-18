80aedc1fbeeadf1d9255aa939955c6847d8d8bcc
humhliao <humhliao@gmail.com>
2024-09-18 21:47:33 +0700

    Practice 76 js

--- Bro Code course/JavaScript/Practice/76/index.js
+++ Bro Code course/JavaScript/Practice/76/index.js
@@ -53,28 +53,25 @@ function walkDog() { // walkDog(callback)
       }, 500);
     });
   }
-  
-  // Thực thi các công việc theo chuỗi với .then() và .catch()
-  
-  walkDog()
-    .then(value => {
-      console.log(value); // In ra kết quả của walkDog nếu thành công
-      return cleanKitchen(); // Sau khi đi dạo chó xong, tiếp tục dọn bếp
-    })
-    .then(value => {
-      console.log(value); // In ra kết quả của cleanKitchen nếu thành công
-      return takeOutTrash(); // Sau khi dọn bếp xong, tiếp tục đổ rác
-    })
-    .then(value => {
-      console.log(value); // In ra kết quả của takeOutTrash nếu thành công
-      console.log("You finished all the chores!"); // Thông báo hoàn thành tất cả công việc
-    })
-    .catch(error => {
-      console.error(error); // Nếu có lỗi xảy ra trong bất kỳ Promise nào, in ra lỗi
-    });
-  
-  // walkDog(() => {
-  //   cleanKitchen(() => {
-  //     takeOutTrash(() => console.log("You finished all the chores"));
-  //   });
-  // });
+
+  async function doChores() {
+
+
+    try {
+
+    const walkDogResult = await walkDog();
+    console.log(walkDogResult);
+    const cleanKitchenResult = await cleanKitchen();
+    console.log(cleanKitchenResult);
+
+    const takeOutTrashResult = await takeOutTrash();
+    console.log(takeOutTrashResult);
+
+    console.log("You finished all the chores!");
+    }
+    catch(error) {
+        console.error(error);
+    }
+  }
+
+  doChores()
