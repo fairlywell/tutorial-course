9bb946a75ef24a9d7eefbdca588fea5266dbd785
Hoang <humhliao@gmail.com>
2024-12-08 21:39:40 +0700

    Add Csharp course

---
+++ Bro Code course/C-Sharp/15/15/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.Write("Enter your name: ");
+            String name = "";
+
+            while (name == "")
+            {
+                Console.Write("Enter your name: ");
+                name = Console.ReadLine();
+            }
+
+            Console.WriteLine("Hello " + name);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Bro Code course/C-Sharp/9/9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int num1 = random.Next(1, 7);
+            int num2 = random.Next(1, 7);
+            int num3 = random.Next(1, 7);
+
+            //double num = random.NextDouble();
+
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine(num3);
+
+            Console.ReadKey();
+        }
+    }
+}
